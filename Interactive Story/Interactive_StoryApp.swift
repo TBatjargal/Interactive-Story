70f2ec0172f936e7ae02aca05692edda43488520
Tsolmon Batjargal <b.tsolmon0421@icloud.com>
2024-12-05 09:28:24 -0600

    Initial Commit

---
+++ Interactive Story/Interactive_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_StoryApp.swift
+//  Interactive Story
+//
+//  Created by Tsolmon Batjargal on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
